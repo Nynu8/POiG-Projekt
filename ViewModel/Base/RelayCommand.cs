71f01fcd38de09ab3d0e8794c9c375c866c3ff28
Nynu <adrianszarnowski8@gmail.com>
2020-07-02 09:06:21 +0200

    Added project structure

---
+++ ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace POiG_Projekt.ViewModel
+{
+    class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+
+        readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+            else
+                this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true : this._canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this._canExecute != null) CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (this._canExecute != null) CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+    }
+}
---
+++ ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace POiG_Projekt.ViewModel.Base
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            this.onPropertyChanged(new[] { name });
+        }
+        protected void onPropertyChanged(params string[] namesOfProperties)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (var prop in namesOfProperties)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                }
+            }
+        }
+    }
+}
