0803a064335769681066af2be211cd8c0349d12e
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2020-07-06 19:11:12 +0200

    created db connection

---
+++ DAL/DBConnection.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POiG_Projekt.DAL
+{
+    class DBConnection
+    {
+        private MySqlConnection cnn;
+        private static DBConnection connection;
+        private DBConnection()
+        {
+            String connetionString = $"host={dbData.HOST};port={dbData.PORT};user id={dbData.USER};password={dbData.PASSWORD};database={dbData.DB};";
+            cnn = new MySqlConnection(@connetionString);
+            cnn.Open();
+        }
+        public static DBConnection Connection
+        {
+            get
+            {
+                if (connection == null)
+                    connection = new DBConnection();
+                return connection;
+            }
+        }
+    }
+}
