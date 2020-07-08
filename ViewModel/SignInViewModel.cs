5f8c80074cc792306e7ab07c416af5b1d9e316c8
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2020-07-09 00:12:32 +0200

    created sign in functionality

--- ViewModel/SignInViewModel.cs
+++ ViewModel/SignInViewModel.cs
@@ -1,4 +1,6 @@
-﻿using POiG_Projekt.ViewModel.Base;
+﻿using POiG_Projekt.Model;
+using POiG_Projekt.View;
+using POiG_Projekt.ViewModel.Base;
 using POiG_Projekt.ViewModel.Navigator;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,101 @@ namespace POiG_Projekt.ViewModel
 {
     class SignInViewModel : ViewModelBase
     {
-        public ICommand ShowMain => new ShowMainWindowCommand();
+        private string nazwaUzytkownika;
+        public string NazwaUzytkownika
+        {
+            get { return nazwaUzytkownika; }
+            set
+            {
+                nazwaUzytkownika = value;
+                OnPropertyChanged(NazwaUzytkownika);
+                Error = "";
+            }
+        }
+
+        private string haslo;
+        public string Haslo
+        {
+            get { return haslo; }
+            set
+            {
+                haslo = value;
+                OnPropertyChanged(Haslo);
+                Error = "";
+            }
+        }
+
+        private string error;
+        public string Error
+        {
+            get { return error; }
+            set
+            {
+                error = value;
+                onPropertyChanged(nameof(Error));
+            }
+        }
+
+        private ICommand showMenu = null;
+        public ICommand ShowMenu
+        {
+            get
+            {
+                if (showMenu == null)
+                {
+                    showMenu = new RelayCommand(
+                        arg =>
+                        {
+                            SignInWindow currentWindow = arg as SignInWindow;
+                            MainWindow mainWindow = new MainWindow();
+                            currentWindow.Close();
+                            mainWindow.Show();
+                        },
+                        arg =>
+                        {
+                            return true;
+                        }
+                        );
+                }
+                return showMenu;
+            }
+        }
+
+        private ICommand login = null;
+        public ICommand Login
+        {
+            get
+            {
+                if (login == null)
+                {
+                    login = new RelayCommand(
+                        arg =>
+                        {
+                            try
+                            {
+                                if (Logowanie.GetID(nazwaUzytkownika, haslo) != 0)
+                                {
+                                    SignInWindow currentWindow = arg as SignInWindow;
+                                    MainWindow mainWindow = new MainWindow();
+                                    currentWindow.Close();
+                                    mainWindow.Show();
+                                }
+                                else
+                                    Error = "Podano zły login lub hasło";
+                            }
+                            catch (Exception e)
+                            {
+                                Error = e.Message;
+                            }
+                        },
+                        arg =>
+                        {
+                            return !(String.IsNullOrEmpty(Haslo) && String.IsNullOrEmpty(NazwaUzytkownika));
+                        }
+                        );
+                }
+                return login;
+            }
+        }
     }
 }
