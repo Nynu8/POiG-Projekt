a1720363d6a19e609af9e7452ba91847ca015838
Nynu <adrianszarnowski8@gmail.com>
2020-07-08 19:24:52 +0200

    Added Student and Profesor models

---
+++ Model/ListaProwadzacy.cs
@@ -0,0 +1,21 @@
+﻿using POiG_Projekt.DAL.Encje;
+using POiG_Projekt.DAL.Repozytoria;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POiG_Projekt.Model
+{
+    class ListaProwadzacy
+    {
+        public List<Prowadzacy> Prowadzacy { get; set; } = new List<Prowadzacy>();
+        public ListaProwadzacy()
+        {
+            var prowadzacy = RepoProwadzacy.PobierzWszystkichProwadzacych();
+            foreach (var p in prowadzacy)
+            {
+                this.Prowadzacy.Add(p);
+            }
+        }
+    }
+}
---
+++ Model/ListaStudentow.cs
@@ -0,0 +1,21 @@
+﻿using POiG_Projekt.DAL.Encje;
+using POiG_Projekt.DAL.Repozytoria;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POiG_Projekt.Model
+{
+    class ListaStudentow
+    {
+        public List<Student> Studenci { get; set; } = new List<Student>();
+        public ListaStudentow()
+        {
+            var studenci = RepoStudenci.PobierzWszyscyStudenci();
+            foreach (var p in studenci)
+            {
+                this.Studenci.Add(p);
+            }
+        }
+    }
+}
