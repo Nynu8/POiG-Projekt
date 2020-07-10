be1d06b5d4e481f1b040712f550bad5b758b5350
Nardox121 <uminski.marcin1@gmail.com>
2020-07-10 11:45:54 +0200

    update encji grupy

--- DAL/Encje/Grupa.cs
+++ DAL/Encje/Grupa.cs
@@ -12,16 +12,19 @@ namespace POiG_Projekt.DAL.Encje
             Id_grupa = sbyte.Parse(reader["id_grupa"].ToString());
             Id_kurs = sbyte.Parse(reader["id_kurs"].ToString());
             Nazwa = reader["nazwa"].ToString();
+            Rok = sbyte.Parse(reader["rok"].ToString());
         }
         public sbyte Id_grupa { get; set; }
         public sbyte Id_kurs { get; set; }
         public string Nazwa { get; set; }
+        public sbyte Rok { get; set; }
         public override bool Equals(object obj)
         {
             var grupa = obj as Grupa;
             if (grupa is null) return false;
             if (Nazwa.ToLower() != grupa.Nazwa.ToLower()) return false;
             if (Id_kurs != grupa.Id_kurs) return false;
+            if (Rok != grupa.Rok) return false;
             return true;
         }
 
